780e539d586044e341cccaa87c13dce6ebc0c749
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-18 12:02:02 +0430

    add slug, override save, email built-in validate

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone
+from django.utils.text import slugify
 
 PUBLISH_CHOICES = [
   ('draft', 'Draft'),
@@ -12,10 +13,11 @@ from .validators import saeedValidate, signValidate
 
 class Product(models.Model):
   title = models.CharField(max_length=15)
+  slug = models.SlugField(null=True, blank=True)
   add_on = models.DateTimeField(auto_now=True)
   update_on = models.DateTimeField(default=timezone.now)
   publish = models.CharField(max_length=15, choices=PUBLISH_CHOICES, default='draft')
-  email = models.CharField(max_length=200, validators=[signValidate, saeedValidate], null=True, blank=True)
+  email = models.EmailField(max_length=200, null=True, blank=True)
 
   # for naming in django admin
   class Meta:
@@ -25,3 +27,9 @@ class Product(models.Model):
 
   def __str__(self):
     return smart_text(self.title)
+
+  # not recommanded
+  def save(self, *args, **kwargs):
+    if not self.slug and self.title:
+      self.slug = slugify(self.title)
+    super().save(*args, **kwargs)
