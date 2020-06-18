909a60617fa3fd3d71b0413991bd1a15f9533633
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-18 11:26:35 +0430

    add two field and unique_together in Meta

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.encoding import smart_text
 
 PUBLISH_CHOICES = [
   ('draft', 'Draft'),
@@ -8,10 +9,15 @@ PUBLISH_CHOICES = [
 
 class Product(models.Model):
   title = models.CharField(max_length=15)
-  add_on = models.DateTimeField()
+  add_on = models.DateTimeField(auto_now=True)
+  update_on = models.DateTimeField(auto_now_add=True)
   publish = models.CharField(max_length=15, choices=PUBLISH_CHOICES, default='draft')
 
   # for naming in django admin
   class Meta:
     verbose_name = 'product'
     verbose_name_plural = 'product'
+    # unique_together = [('title', 'slug')]
+
+  def __str__(self):
+    return smart_text(self.title)
