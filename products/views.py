0fd93f2ae8d5ea45393c54f707bd994d5f97477d
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-18 19:49:56 +0430

    template views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, View
+from django.views.generic.base import ContextMixin, TemplateResponseMixin
+class AboutTemplateView(TemplateView):
+  template_name = 'products/template.html'
 
-# Create your views here.
+  def get_context_data(self, *args, **kwargs):
+    context = super(AboutTemplateView, self).get_context_data(*args, **kwargs)
+    context["about"] = "about context"
+    return context
+
+class MyTemplateView(ContextMixin, TemplateResponseMixin, View):
+  
+  def get(self, request, *args, **kwargs):
+    context = self.get_context_data(**kwargs)
+    context["title"] = "this is from 'MyTemplateView'"
+    return self.render_to_response(context)
