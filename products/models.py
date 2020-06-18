a1598ae2c2997d7c0178e7d902c71cad11472f84
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-18 11:14:04 +0430

    Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+PUBLISH_CHOICES = [
+  ('draft', 'Draft'),
+  ('publish', 'Publish'),
+  ('private', 'Private'),
+]
+
+class Product(models.Model):
+  title = models.CharField(max_length=15)
+  add_on = models.DateTimeField()
+  publish = models.CharField(max_length=15, choices=PUBLISH_CHOICES, default='draft')
+
+  # for naming in django admin
+  class Meta:
+    verbose_name = 'product'
+    verbose_name_plural = 'product'
