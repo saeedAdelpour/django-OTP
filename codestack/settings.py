a8c13bfc856cd5b72343a0fb8e97a7312abdfa5e
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-19 19:44:44 +0430

    install otpapi app

--- codestack/settings.py
+++ codestack/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'accounts.apps.AccountsConfig',
     'otpverify.apps.OtpverifyConfig',
+    'otpapi.apps.OtpapiConfig',
 ]
 
 MIDDLEWARE = [
