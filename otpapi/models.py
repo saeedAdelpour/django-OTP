0bbed52fd9f2790523c5902c550d4ff488718603
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-21 00:42:41 +0430

    add logged_in, getter and setter into model

--- otpapi/models.py
+++ otpapi/models.py
@@ -5,12 +5,22 @@ class Client(models.Model):
   name = models.CharField(max_length=10)
   number = models.IntegerField(unique=True)
   otp = models.IntegerField(null=True)
+  logged_in = models.BooleanField(default=False)
 
   def set_otp(self):
     self.otp = int(random() * 10 ** 4)
   
   def set_none_otp(self):
     self.otp = None
+  
+  def login(self):
+    self.logged_in = True
+
+  def logout(self):
+    self.logged_in = False
+
+  def is_auth(self):
+    return self.logged_in
 
   def __str__(self):
     return "-".join([item for item in ["client_object", self.name, str(self.number)] if item])
