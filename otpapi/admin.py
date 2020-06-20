6e763bae5f7a659574bfdd76c94a62218172d6e7
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-21 00:42:00 +0430

    change admin panel

--- otpapi/admin.py
+++ otpapi/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Client, Session
 
 class ClientManager(admin.ModelAdmin):
-  list_display = ["name", "number", "otp"]
+  list_display = ["name", "number", "otp", "logged_in"]
 
 class SessionManager(admin.ModelAdmin):
   list_display = ["client", "user_agent"]
