56152cb79ebb8b367053c150089446450949822a
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-21 21:21:38 +0430

    install rest framework

--- codestack/settings.py
+++ codestack/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'products.apps.ProductsConfig',
     'accounts.apps.AccountsConfig',
     'otpverify.apps.OtpverifyConfig',
