d5ff942feca6860c27ca159a934e7905173b9ffc
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-22 19:53:21 +0430

    handle InvalidSignatureError in middleware

--- otpapi/middleware.py
+++ otpapi/middleware.py
@@ -3,6 +3,7 @@ from psycopg2.errors import NumericValueOutOfRange
 from django.http.response import JsonResponse
 from django.core.exceptions import FieldDoesNotExist
 from .models import Client
+from jwt.exceptions import InvalidSignatureError
 
 class SimpleMiddleware:
   def __init__(self, get_response):
@@ -13,9 +14,9 @@ class SimpleMiddleware:
     # Code to be executed for each request before
     # the view (and later middleware) are called.
 
-    print("__call__: i am before get_response")
+    # print("__call__: i am before get_response")
     response = self.get_response(request)
-    print("__call__: i am after get_response")
+    # print("__call__: i am after get_response")
 
     # Code to be executed for each request/response after
     # the view is called.
@@ -23,7 +24,8 @@ class SimpleMiddleware:
     return response
 
   def process_view(self, request, view_func, view_args, view_kwargs):
-    print("process_view: in process_view")
+    pass
+    # print("process_view: in process_view")
   ValueError
   def process_exception(self, request, exception):
     exception_type = type(exception)
@@ -31,11 +33,17 @@ class SimpleMiddleware:
     exception_message = None
     if exception_type is DataError:
       exception_message = "out of range data"
+
     elif exception_type is FieldDoesNotExist:
       exception_message = str(exception)
+
     elif exception_type is ValueError:
       exception_message = "this data is invalid"
+
     elif exception_type is Client.DoesNotExist:
       exception_message = str(exception)
+
+    elif exception_type is InvalidSignatureError:
+      exception_message = "invalid token"
     
     return JsonResponse({"error_type": str(type(exception)), "error_message": exception_message})
