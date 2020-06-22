74ad8ffe026523d929ff7948c8006150a1a61c21
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-23 00:22:55 +0430

    SnippetList view

---
+++ restapi/class_base_views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from .models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
+class SnippetList(APIView):
+  def get(self, request, format=None):
+    snippets = Snippet.objects.all()
+    serializer = SnippetSerializer(snippets, many=True)
+    return Response(serializer.data)
+  
+  def post(self, request, format=None):
+    serializer = SnippetSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
