7cb66c231c48045c8d26921bc4864c374f4a8631
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-24 14:08:53 +0430

    new endpoints for see users

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .class_base_views import SnippetList, SnippetDetail
+from .class_base_views import SnippetList, SnippetDetail, UserList, UserDetail
 from . import views
 
 urlpatterns = [
     path('snippets/', SnippetList.as_view()),
     path('snippets/<int:pk>/', SnippetDetail.as_view()),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
