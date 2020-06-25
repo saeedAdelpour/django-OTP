5e6968c404009ce8b0d2536ad042b92b734e2498
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-25 12:42:44 +0430

    fixing api_root bug

--- restapi/urls.py
+++ restapi/urls.py
@@ -4,9 +4,9 @@ from .class_base_views import SnippetList, SnippetDetail, UserList, UserDetail
 from . import views
 
 urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
+    path('snippets/', SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', SnippetDetail.as_view()),
-    path('users/', UserList.as_view()),
+    path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetail.as_view()),
 ]
 
