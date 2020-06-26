fd03097017065e5d966bf519f4c8afe1617513bd
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 17:31:59 +0430

    create model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=20)
+  description = models.TextField(max_length=200, null=True)
+  created_on = models.DateField(auto_now_add=True)
