1a31ab4cb2dcadd21cdb8fa66bc7bc328791d013
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 13:29:32 +0430

    add permissions

--- drink/views.py
+++ drink/views.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Drink
 from .serializer import DrinkSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
+
 class DrinkViewSet(viewsets.ModelViewSet):
   queryset = Drink.objects.all()
   serializer_class = DrinkSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
