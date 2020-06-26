0d36174179273d4220f93e74cf7738a828245292
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 18:10:26 +0430

    register and add str methd

--- core/models.py
+++ core/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
   title = models.CharField(max_length=20)
   description = models.TextField(max_length=200, null=True)
   created_on = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
