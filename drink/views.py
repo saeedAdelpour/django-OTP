d93d454a79de08fe22b7f86f8b090158ad4cc246
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 13:30:24 +0430

    custom get_queryset for filtering through items

--- drink/views.py
+++ drink/views.py
@@ -11,6 +11,21 @@ class DrinkViewSet(viewsets.ModelViewSet):
   serializer_class = DrinkSerializer
   permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+  def get_queryset(self):
+    request_name_filter = self.request.query_params.get("name")
+    request_addition_filter = self.request.query_params.get("addition")
+
+    if request_name_filter and request_addition_filter:
+      queryset = Drink.objects.filter(name__icontains=request_name_filter, addition=request_name_filter)
+    elif request_name_filter:
+      queryset = Drink.objects.filter(name__icontains=request_name_filter)
+    elif request_addition_filter:
+      queryset = Drink.objects.filter(addition=request_name_filter)
+    else:
+      queryset = Drink.objects.all()
+
+    return queryset
+
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
 
