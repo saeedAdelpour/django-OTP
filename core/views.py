44135674a2febd44fe20f662bafb8cb0a269fe45
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 18:09:43 +0430

    add form to view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import PostModelForm
 
 def my_form(request):
-  pass
+  form = PostModelForm(request.POST or None)
+  if form.is_valid():
+    form.save()
+
+  return render(request, 'core/forms.html', {"form": form})
