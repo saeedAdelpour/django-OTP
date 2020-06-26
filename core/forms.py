a0244a07fadda0d82c6bb85b18b6ebf54a0b3a24
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 18:49:11 +0430

    form customizations

--- core/forms.py
+++ core/forms.py
@@ -8,6 +8,16 @@ class PostModelForm(forms.ModelForm):
     model = Post
     fields = ["title", "description"]
 
+    labels = {
+      "title": "enter title",
+      "description": "enter description",
+    }
+
+    help_texts = {
+      "title": "enter title",
+      "description": "enter description",
+    }
+
   def clean_title(self, *args, **kwargs):
     title = self.cleaned_data.get("title")
     if(len(title) < 3):
@@ -19,3 +29,11 @@ class PostModelForm(forms.ModelForm):
     if len(description) < 5:
       raise ValidationError("error: len < 5")
     return description
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    
+    for field in self.fields.values():
+      field.error_messages = {
+        "max_length": "my custom error in field {}".format(field.label)
+      }
