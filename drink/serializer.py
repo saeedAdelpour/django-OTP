5c5e988b3432a24a4bbc180f5185645e5bcfadc4
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 13:49:53 +0430

    UserSerializer class

--- drink/serializer.py
+++ drink/serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Drink
 
@@ -7,3 +8,10 @@ class DrinkSerializer(serializers.ModelSerializer):
   class Meta:
     model = Drink
     fields = ["id", "name", "addition", "owner", "url"]
+
+class UserSerializer(serializers.ModelSerializer):
+  drinks = serializers.HyperlinkedRelatedField(many=True, view_name="drink-detail", read_only=True)
+
+  class Meta:
+    model = User
+    fields = ["id", "username", "url", "drinks"]
