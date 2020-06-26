6d7dad51cb13faa226b910ab28a9d1fb6110ccc3
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 13:24:39 +0430

    add drink model

--- drink/models.py
+++ drink/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+addition_choices = [(item, item) for item in ["mocha", "soy"]]
+
+class Drink(models.Model):
+  name = models.CharField(max_length=15)
+  addition = models.CharField(max_length=15, choices=addition_choices)
+  created = models.DateField(auto_now_add=True)
+  owner = models.ForeignKey(User, related_name='drinks',on_delete=models.CASCADE)
