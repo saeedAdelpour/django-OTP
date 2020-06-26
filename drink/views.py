5b5795d125a8cfe6c8297fea1daaa7d273f9f685
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 13:28:44 +0430

    add UserViewSet class

--- drink/views.py
+++ drink/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from .serializer import DrinkSerializer
+from rest_framework import viewsets
+from .models import Drink
+from .serializer import DrinkSerializer, UserSerializer
 class DrinkViewSet(viewsets.ModelViewSet):
   queryset = Drink.objects.all()
   serializer_class = DrinkSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
