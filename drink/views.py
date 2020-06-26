2eddea004e62f9e13811b5fc0c7e27dbb221f83b
saeedadelpour <saeed.adelpour99@gmail.com>
2020-06-26 13:27:40 +0430

    add DrinkViewSet class

--- drink/views.py
+++ drink/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .serializer import DrinkSerializer
+class DrinkViewSet(viewsets.ModelViewSet):
+  queryset = Drink.objects.all()
+  serializer_class = DrinkSerializer
 
 # Create your views here.
